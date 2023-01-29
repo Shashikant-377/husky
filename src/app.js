2c02286611d00c0661c0c320626cc133eec921e4
shashi <shahikantsethiya50896@gmail.com>
2023-01-30 00:36:28 +0530

    second commit

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log("hello")
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (req,res)=>{
+res.send("this is live node app")
+})
+
+app.listen(5000,()=>{
+    console.log("server live at 5000")
+})
